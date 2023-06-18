a6aeb03498aa810407c4c1cd2a9568a85170d1b3
adimit078 <42521995+adimit078@users.noreply.github.com>
2023-06-18 11:43:21 -0700

    Initial Commit

---
+++ SwiftUITabBar/SwiftUITabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITabBarApp.swift
+//  SwiftUITabBar
+//
+//  Created by Aditya Mittal on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
